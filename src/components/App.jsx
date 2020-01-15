846bac96bf86f4ee32e21f099eb600d3ccb48a48
S-Masakatsu <s.masakatsu197@gmail.com>
2020-01-15 20:17:26 +0900

    [add]App

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Routes from 'components/Routes'
+import Footer from 'components/layouts/Footer'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Routes />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
