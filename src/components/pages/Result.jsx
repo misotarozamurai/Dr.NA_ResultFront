92c6221f989bff18d167de9cb12f390b4abbc7ba
S-Masakatsu <s.masakatsu197@gmail.com>
2020-01-17 11:36:28 +0900

    [add]redux output data

--- src/components/pages/Result.jsx
+++ src/components/pages/Result.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import { getNameText } from 'redux/selectors'
+import React, { Component } from 'react'
+import { getNameText } from 'redux/selectors/name'
 import { connect } from 'react-redux'
 
-const Result = ({name}) => {
-  console.log(name)
-  return (
-    <div>
-      <p>{name}</p>
-    </div>
-  );
+class Result extends Component {
+  render() {
+    return (
+      <div>
+        <p className='user_name'>ユーザー：{this.props.name}</p>
+        <p className='user_name'>日付：{this.props.data.datatime}</p>
+        <p className='user_name'>脈平均：{this.props.data.pulse.avg}</p>
+        <p className='user_name'>病名：{this.props.data.sick.name}</p>
+        <p className='user_name'>説明：{this.props.data.sick.message}</p>
+      </div>
+    );
+  }
 }
 
-const MapStateToProps = state => {
-  const name = getNameText(state);
-  return {name};
-}
+const MapStateToProps = state => state.name
 
 export default connect(MapStateToProps)(Result);
