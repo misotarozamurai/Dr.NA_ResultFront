ac92e2dae6a4aaa59e51331e478b06ba51c3ad0b
S-Masakatsu <s.masakatsu197@gmail.com>
2020-01-18 16:24:13 +0900

    [updata]display text line

--- src/config/text.js
+++ src/config/text.js
@@ -1,7 +1,7 @@
 // ----- Text for display -----
 // Top Page
 export const INPUT_NAME = {
-  label: 'ニックネームを入力してボタンを押してください。', 
+  label_line: 'ニックネームを入力してボタンを押してください', 
   placeholder: 'ニックネームを入力'
 }
 
@@ -9,12 +9,14 @@ export const BUTTON_SEND = 'Send Name'
 
 // Result Page
 export const DISPLAY_NAME_TEXT = '{name}さん、DNA診断お疲れ様でした！<br>私から結果の方をお伝えするね。'
+
 export const DISPLAY_RESULT_AVG = {
   avg: '脈の平均は、{avg}bpm だったよ。',
   standard: '一般的には基準値だね！',
   slow: '一般的には遅い数値かな。',
   early: '一般的には早い数値だよ。'
 }
+
 export const DISPLAY_RESULT_SICK_NAME = {
   nameline: '{name}さんに投与したナノマシンが解析した結果、',
   sickline: '{name}さんは、将来的に{sick}になると診断されました。'
