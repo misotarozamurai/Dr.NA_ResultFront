50e2e93e63117056f33764bde568f270c1d43c81
S-Masakatsu <s.masakatsu197@gmail.com>
2020-01-18 11:25:17 +0900

    [updata]import css module

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,19 +1,22 @@
 import React, { Component } from 'react'
-// import Logo from 'components/layouts/Logo'
 import { withRouter } from 'react-router-dom'
+// ----- config -----
 import { RESULT_PAGE } from 'config/route'
+// ----- JSX -----
+import Logo from 'components/layouts/Logo'
 import TopSendName from 'components/ui/TopSendName'
+// ----- style -----
+import Style from 'stylesheet/style.module.sass'
 
 class Top extends Component {
-
   handleClick = () => {
     this.props.history.push(RESULT_PAGE);
   };
   
   render() {
     return (
-      <div className="top_page">
-        {/* <Logo /> */}
+      <div className={Style.top_page}>
+        <Logo />
         <TopSendName transition={this.handleClick}/>
       </div>
     );
